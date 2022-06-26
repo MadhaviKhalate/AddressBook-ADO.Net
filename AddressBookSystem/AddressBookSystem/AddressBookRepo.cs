ecf0712302ced5a409cbe0aa3d592c7f2b6254fd
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-26 15:49:17 +0530

    [Madhavi] Adding Data

---
+++ AddressBookSystem/AddressBookSystem/AddressBookRepo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBookRepo
+    {
+        public static string connectionString = "Data Source=DESKTOP-7KANMDE\\SQLEXPRESS;Initial Catalog=ADDRESSBOOK_SERVICE;Integrated Security=True;" +
+            "Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public void AddData(AddressBook_Model model)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            using (connection)
+            {
+                try
+                {
+                    SqlCommand command = new SqlCommand("AddContacts", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@FIRST_NAME", model.firstName);
+                    command.Parameters.AddWithValue("@LAST_NAME", model.lastName);
+                    command.Parameters.AddWithValue("@ADDRESS", model.address);
+                    command.Parameters.AddWithValue("@CITY", model.city);
+                    command.Parameters.AddWithValue("@STATE", model.state);
+                    command.Parameters.AddWithValue("@ZIP_CODE", model.zipCode);
+                    command.Parameters.AddWithValue("@PHONE_NUMBER", model.phone);
+                    command.Parameters.AddWithValue("@EMAIL", model.email);
+                    command.Parameters.AddWithValue("@TYPE", model.type);
+                    connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    connection.Close();
+                    Console.WriteLine("Data Added Successfully");
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AddressBookRepo getMethod = new AddressBookRepo();
+            AddressBook_Model model = new AddressBook_Model();
+            Console.WriteLine("Enter First Name");
+            model.firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            model.lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address ");
+            model.address = Console.ReadLine();
+            Console.WriteLine("Enter City ");
+            model.city = Console.ReadLine();
+            Console.WriteLine("Enter State ");
+            model.state = Console.ReadLine();
+            Console.WriteLine("Enter Zip Code ");
+            model.zipCode = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Phone ");
+            model.phone = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Email ");
+            model.email = Console.ReadLine();
+            Console.WriteLine("Enter Type ");
+            model.type = Console.ReadLine();
+            getMethod.AddData(model);
+        }
+    }
+}
